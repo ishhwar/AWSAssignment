91bfb626e2bec40f6a83db815226fa5fcebcb77b
ishwar-13280 <ishwar.singh@zohocorp.com>
2024-06-02 13:32:00 +0530

    aws assignment fix commit

--- src/main/java/com/example/aws/sdk/SdkApplication.java
+++ src/main/java/com/example/aws/sdk/SdkApplication.java
@@ -14,31 +14,6 @@ public class SdkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SdkApplication.class, args);
-		Ec2Client ec2 = Ec2Client.builder()
-				.credentialsProvider(StaticCredentialsProvider.create(
-				AwsBasicCredentials.create("AKIAX5XSI5UT4M674AFL", "SEUYFy4LLDLvDG184lP2qM+PE/NFmi5MUCH3Wf7I")))
-				.region(software.amazon.awssdk.regions.Region.AP_SOUTH_1)
-				.build();
-		S3Client s3Client = S3Client.builder()
-				.region(Region.of(String.valueOf(Region.AP_SOUTH_1)))
-				.credentialsProvider(StaticCredentialsProvider.create(
-						AwsBasicCredentials.create("AKIAX5XSI5UT4M674AFL", "SEUYFy4LLDLvDG184lP2qM+PE/NFmi5MUCH3Wf7I")))
-				.build();
-
-//		ListObjectsV2Request request2 = ListObjectsV2Request.builder().bucket("nimesaassignmentbucket1").build();
-//
-//		ListObjectsV2Response result;
-//		do {
-//			result = s3Client.listObjectsV2(request2);
-//			for (S3Object s3Object : result.contents()) {
-////				AwsComponents resource = new AwsComponents();
-////				resource.setJobId(jobId);
-////				resource.setResourceType("S3Object");
-////				resource.setDetails(s3Object.key());
-////				awscomponentsrepository.save(resource);
-//			}
-//			request2 = request2.toBuilder().continuationToken(result.nextContinuationToken()).build();
-//		} while (result.isTruncated());
 
 	}
 
